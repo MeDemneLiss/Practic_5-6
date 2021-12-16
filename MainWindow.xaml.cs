042c0a3e18dae539821c53a48fc42f3cec526de3
Andreiko_0 <92972300+MeDemneLiss@users.noreply.github.com>
2021-12-16 13:57:39 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows;
+
+namespace Practic_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Car carNumberOne = new Car ();
+        Car carNumberTwo = new Car();
+
+        private void ChangeParameters_Click(object sender, RoutedEventArgs e)
+        {
+            if (firstCar.IsChecked == true)
+            {
+                FillInformation(carNumberOne);
+            }
+            else
+            {
+                FillInformation(carNumberTwo);
+            }
+            parameterOutput.Text = carNumberOne.ShowInformation() + "\n \n" + carNumberTwo.ShowInformation();
+        }
+
+        private void FillInformation(Car vehicle)
+        {
+            if (tredemark.Text != string.Empty)
+            {
+                vehicle.SetParams(tredemark.Text);
+
+            }
+
+            if (int.TryParse(power.Text, out int powerVaue))
+            {
+                if (powerVaue < 0)
+                {
+                    MessageBox.Show("Значение поля Мощность должно быть положительным ", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    vehicle.SetParams(powerVaue);
+                }
+            }
+
+            if (double.TryParse(numberCylinders.Text, out double numberCylindersValue))
+            {
+                if (numberCylindersValue < 0)
+                {
+                    MessageBox.Show("Значение поля кол-во цилиндров должно быть положительным", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    vehicle.SetParams(numberCylindersValue);
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Самсаков Андрей Александрович ИСП-31\nПрактическая работа№5-6\n - Создать базовый класс Саr (машина), характеризуемый торговой маркой (строка), числом цилиндров, мощностью.Создать необходимые методы и свойства. Создать перегруженные методы SetParams, для установки параметров автомобиля. \n - Использовать базовый класс Саr (машина), характеризуемый торговой маркой (строка), числом цилиндров, мощностью.Разработать операции для определения крутости машин.Машина считается круче, если у одной машины количество цилиндров и мощность больше чем у другой машины или при равенстве одного из параметров второй параметр больше.Разработать операцию увеличение мощности на 1. ", "Информация о программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void IncreasePower_Click(object sender, RoutedEventArgs e)
+        {
+            if (firstCar.IsChecked == true)
+            {
+                carNumberOne++;
+                power.Text = carNumberOne.Power.ToString();
+            }
+            if (secondCar.IsChecked == true)
+            {
+                carNumberTwo++;
+                power.Text = carNumberTwo.Power.ToString();
+            }
+            parameterOutput.Text = carNumberOne.ShowInformation() + "\n \n" + carNumberTwo.ShowInformation();
+        }
+
+        private void Comparison_Click(object sender, RoutedEventArgs e)
+        {
+            if (carNumberOne > carNumberTwo)
+            {
+                MessageBox.Show("Первая машина круче второй", "Итог сравнения", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (carNumberOne < carNumberTwo)
+            {
+                MessageBox.Show("Вторая машина круче первой", "Итог сравнения", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Уровень крутости машинок равный", "Итог сравнения", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void FirstCar_Checked(object sender, RoutedEventArgs e)
+        {
+            numberCylinders.Clear();
+            tredemark.Clear();
+            power.Clear();
+        }
+
+        private void SecondCar_Checked(object sender, RoutedEventArgs e)
+        {
+            numberCylinders.Clear();
+            tredemark.Clear();
+            power.Clear();
+        }
+    }
+}
---
+++ Саr.cs
@@ -0,0 +1,58 @@
+﻿namespace Practic_5
+{
+    class Car
+    {
+        public string Tredemark { get; set; }
+        public int Power { get; set; }
+        public double NumberCylinders { get; set; }
+
+        public Car()
+        {
+            Tredemark = string.Empty;
+            Power = 0;
+            NumberCylinders = 0;
+        }
+        public Car SetParams(string inName)
+        {
+            Tredemark = inName;
+            return this;
+        }
+        public Car SetParams(double inWeight)
+        {
+            NumberCylinders = inWeight;
+            return this;
+        }
+        public Car SetParams(int inHowOld)
+        {
+            Power = inHowOld;
+            return this;
+        }
+
+        public string ShowInformation()
+        {
+            string info = "Торговая марка - " + Tredemark + "\n" + "Мощность - " + Power + " л.с.\n" + "Колличество цилиндров - " + NumberCylinders;
+            return info;
+        }
+
+        //6 Практическая
+        public static Car operator ++(in Car carNumberOne)
+        {
+            carNumberOne.Power += 1;
+            return carNumberOne;
+        }
+
+        public static bool operator >(Car carNumberOne, Car carNumberTwo)
+        {
+            bool result = (carNumberOne.Power > carNumberTwo.Power && carNumberOne.NumberCylinders > carNumberTwo.NumberCylinders) || (carNumberOne.Power > carNumberTwo.Power && carNumberOne.NumberCylinders == carNumberTwo.NumberCylinders) || (carNumberOne.Power == carNumberTwo.Power && carNumberOne.NumberCylinders > carNumberTwo.NumberCylinders);
+            return result;
+        }
+
+        public static bool operator <(Car carNumberOne, Car carNumberTwo)
+        {
+            bool result = carNumberOne.Power < carNumberTwo.Power && carNumberOne.NumberCylinders < carNumberTwo.NumberCylinders || carNumberOne.Power < carNumberTwo.Power && carNumberOne.NumberCylinders == carNumberTwo.NumberCylinders || carNumberOne.Power == carNumberTwo.Power && carNumberOne.NumberCylinders < carNumberTwo.NumberCylinders;
+            return result;
+        }
+    }
+
+}
+
